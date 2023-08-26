403627d728c7562c600cd848bdc3b2b98037e3dd
alikamran44 <ali.kamran4477696@gmail.com>
2023-08-26 20:53:06 +0500

    'last'

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,8 +4,12 @@ interface ImportMetaEnv {
   readonly VITE_APP_TITLE: string;
   readonly VITE_LRT_OR_RTL: "rtl" | "ltr";
   readonly VITE_APP_NAME: string;
+  readonly VITE_BASE_API: any;
+  readonly VITE_PRODUCTION_BASE_API: any;
+  readonly VITE_PRODUCTION_DOMAIN: any;
+  readonly VITE_DEVELOPMENT_DOMAIN: any;
   // more env variables...
-}
+} 
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
