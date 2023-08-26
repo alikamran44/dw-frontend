35d3cc305f0ad79d1a5031a795901b4c72204297
alikamran44 <ali.kamran4477696@gmail.com>
2023-08-26 21:31:59 +0500

    'test'

--- src/containers/PageHome/PageHomeDemo6.tsx
+++ src/containers/PageHome/PageHomeDemo6.tsx
@@ -136,19 +136,7 @@ const PageHomeDemo6: React.FC = () => {
 
   return (
     <div className="nc-PageHomeDemo6 relative [ nc-section-rounded-md ]">
-      <Helmet>
-        <title>Home || Blog Magazine React Template</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600&display=swap"
-          rel="stylesheet"
-        ></link>
-      </Helmet>
+      
       {/* ======== ALL SECTIONS ======== */}
 
 
