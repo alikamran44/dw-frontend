776e65e7d4f6c4dbc2d25db4874a146abc0fe102
alikamran44 <ali.kamran4477696@gmail.com>
2023-10-03 09:08:15 +0500

    'changes'

--- src/containers/PageSingle/SingleTitle.tsx
+++ src/containers/PageSingle/SingleTitle.tsx
@@ -19,7 +19,7 @@ const SingleTitle: FC<SingleTitleProps> = ({
       {
         title ? (
           <h1 className={className + " " + mainClass + " max-w-4xl "} title={title}>
-            {title}Khannnnnnnnnnnn oyeee
+            {title}
 
           </h1>)
           : loading && (
